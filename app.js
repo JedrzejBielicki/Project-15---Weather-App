c167a43a37d4f7ede24d7de2a1554a3fd4e692e8
Jędrzej <jedrzejb123@gmail.com>
2020-07-09 09:39:41 +0200

    add week section

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ window.addEventListener("load", () => {
   let temp = document.querySelector(".temp");
   let desc = document.querySelector(".desc");
   let tzone = document.querySelector(".timezone");
+  let weekTemp = document.querySelectorAll(".week-temp h3");
+  let weekSky = document.querySelectorAll(".week-sky h3");
   let long;
   let lat;
 
@@ -36,14 +38,31 @@ window.addEventListener("load", () => {
             dayTemp.push(`${Math.floor(daily - 273.15)} C`);
           }
 
+          let daySky = [];
+
+          for (let i = 0; i < data.daily.length; i++) {
+            let daily = data.daily[i].weather[0].description;
+            daySky.push(daily);
+          }
+
           console.log(dayTemp);
           console.log(temperature);
           console.log(description);
           console.log(timezone);
+          console.log(weekTemp);
 
           temp.textContent = temperature;
           desc.textContent = description;
           tzone.textContent = `Timezone: ${timezone}`;
+
+          add = (length, array, data) => {
+            for (let i = 0; i < length.length; i++) {
+              array[i].textContent = data[i];
+            }
+          };
+
+          let weekTempArr = add(weekTemp, weekTemp, dayTemp);
+          let weekSkyArr = add(weekSky, weekSky, daySky);
         });
     });
   } else {
